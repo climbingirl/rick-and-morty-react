1dd75cbf1618fb7c6920afa98705bf0bc3a65583
climbingirl <isvetlanaivanova@yandex.ru>
2023-03-20 01:30:04 +0700

    feat: add styles for 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './NotFound.scss';
 
 class NotFound extends React.Component {
   render(): React.ReactNode {
     return (
       <div className="not-found">
         <div className="not-found__container container">
-          <h2 className="not-found__title title-h2">404</h2>;
-          <div className="not-found__text">Page not found</div>;
+          <div className="not-found__title">404</div>
+          <div className="not-found__text">Page not found</div>
         </div>
       </div>
     );
